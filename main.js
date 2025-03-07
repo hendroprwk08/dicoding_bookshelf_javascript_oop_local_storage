1ada57db42a93354324e2fa611bfb188eef65658
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2025-03-07 11:20:15 +0700

    start

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
+import { BookManager as Manager } from './book_manager.js';
+
 // Do your work here...
 console.log('Hello, world!');
 
 // catatan
 // Inisialisasi BookManager
-const bookManager = new BookManager('BOOKS_DATA');
+const manager = new Manager('BOOKS_DATA');
 
 // Contoh penggunaan
-const newBook = bookManager.create('Harry Potter', 'J.K. Rowling', 1997, false);
+const newBook = manager.create('Harry Potter', 'J.K. Rowling', 1997, false);
 console.log('Buku baru:', newBook);
 
-const book = bookManager.read(newBook.id);
+const book = manager.read(newBook.id);
 console.log('Buku dengan ID:', book);
 
-const updatedBook = bookManager.update(newBook.id, { isComplete: true });
+const updatedBook = manager.update(newBook.id, { isComplete: true });
 console.log('Buku setelah update:', updatedBook);
 
-bookManager.delete(newBook.id);
-console.log('Buku setelah dihapus:', bookManager.getAllBooks());
+manager.delete(newBook.id);
+console.log('Buku setelah dihapus:', manager.getAllBooks());
 
 // Mendapatkan semua buku
-const allBooks = bookManager.getAllBooks();
+const allBooks = manager.getAllBooks();
 console.log('Semua buku:', allBooks);
 
 // Mendapatkan buku yang sudah selesai dibaca
-const completeBooks = bookManager.getCompleteBooks();
+const completeBooks = manager.getCompleteBooks();
 console.log('Buku yang sudah selesai dibaca:', completeBooks);
 
 // Mendapatkan buku yang belum selesai dibaca
-const uncompleteBooks = bookManager.getUncompleteBooks();
+const uncompleteBooks = manager.getUncompleteBooks();
 console.log('Buku yang belum selesai dibaca:', uncompleteBooks);
